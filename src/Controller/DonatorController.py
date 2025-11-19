91285f501d5cda4891ab7d9b4d1a609efaefde72
João Butzen <butzenjvlb@gmail.com>
2025-11-19 19:35:50 -0300

    feat/ Add view to receivers

--- src/Controller/DonatorController.py
+++ src/Controller/DonatorController.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, HTTPException, Request
-from src.Model import CadastrateModel, LoginModel
-from src.Helper.SignInHelper import SignInHelper
-from src.Helper.TokenHelper import TokenHelper
+from fastapi import APIRouter, HTTPException, Request, Depends
+from src.Model.ListReceiversRequestModel import ListReceiversRequestModel
+from src.Helper.ReceiversHelper import ReceiversHelper
+from src.Helper.SecurityHelper import get_current_user_from_token
 
 class DonatorController:
     
@@ -10,3 +10,16 @@ class DonatorController:
     @router.get("/")
     async def get_donator():
         return {"message": "Donator endpoint is working!"}
+    
+    @router.post("list_receivers")
+    async def list_receivers(request: ListReceiversRequestModel,
+        user: str = Depends(get_current_user_from_token)):
+
+        if user != "doador":
+            raise HTTPException(status_code=403, detail="Unauthorized access: Only donators can access this endpoint")
+        try:
+            helper = ReceiversHelper()
+            receivers = helper.get_receivers(request)
+            return {"receivers": receivers}
+        except Exception as e:
+            raise HTTPException(status_code=404, detail=f"Error fetching receivers: {e}")
